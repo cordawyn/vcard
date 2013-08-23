6876d19440de6086998b50c15bcc4739c76ea5ab
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-08-23 22:53:22 +0300

    removed debugging stuff

--- vcard.scm
+++ vcard.scm
@@ -42,7 +42,6 @@
           (string-upcase (symbol->string param-name))
           (if (pair? param-values)
               (let ((type (assoc-ref (cadr param-values) 'value)))
-                (display (cadr param-values))
                 (string-append
                  ";" (vcard-properties->string (cadr param-values))
                  ":" (serialize-vcard-value (or (and type (string->symbol (string-downcase type))) 'text) (car param-values))))
