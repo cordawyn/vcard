8c1ad094af03b1b8867738413a9eea7e900c79ec
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-08-24 14:37:28 +0300

    serialization of different value types

--- vcard.scm
+++ vcard.scm
@@ -46,8 +46,8 @@
               (let ((type (assoc-ref (cadr param-values) 'value)))
                 (string-append
                  ";" (vcard-properties->string (cadr param-values))
-                 ":" (serialize-vcard-value (or (and type (string->symbol (string-downcase type))) 'text) (car param-values))))
-              (string-append ":" (serialize-vcard-value 'text param-values)))
+                 ":" (serialize-vcard-value (or type "text") (car param-values))))
+              (string-append ":" (serialize-vcard-value "text" param-values)))
           "\n\r")))
      (vcard-parameters vc))
     "")
@@ -60,37 +60,55 @@
       (let ((name (car attr)) (value (cdr attr)))
         (string-append
          (string-upcase (symbol->string name)) "="
-         (stringify value))))
+         (format #f "~a" value))))
     properties)
    ";"))
 
 (define (serialize-vcard-value type val)
-  (case type
-    ;; TODO
-    ('uri
+  (cond
+    ;; TODO: value lists (e.g. (10, 20, 30) => "10,20,30")
+    ((equal? type "uri")
      (uri->string val))
-    ('date
+    ((equal? type "date")
      (if (date? val)
          (date->string val "~Y~m~d")
          ;; TODO: date as an incomplete triple?
          (write val)))
-    ('time
+    ((equal? type "time")
      (cond ((date? val)
-            (date->string val "~2"))
+            (date->string val "~H~M~S~z"))
            ((time? val)
             (error "Handling of time objects is not implemented yet. Try date."))
            ;; TODO: incomplete time/date (hours or minutes only, etc.)
            (else
             (write val))))
+    ((equal? type "date-time")
+     (if (date? val)
+         (date->string val "~Y~m~dT~H~M~S~z")
+         ;; TODO: incomplete combinations of date and time
+         (write val)))
+    ((equal? type "date-and-or-time")
+     (if (date? val)
+         (date->string val "~Y~m~dT~H~M~S")
+         ;; TODO: incomplete combinations of date and time
+         (write val)))
+    ((equal? type "timestamp")
+     (date->string val "~Y~m~dT~H~M~S~z"))
+    ((equal? type "boolean")
+     (or (and val "true") "false"))
+    ((equal? type "integer")
+     (number->string val))
+    ((equal? type "float")
+     (number->string val))
+    ((equal? type "utc-offset")
+     ;; TODO
+     (error "Not implemented."))
+    ((equal? type "language-tag")
+     val)
     (else
      (escape-vcard-value
-      (stringify val)))))
+      (format #f "~a" val)))))
 
 (define (escape-vcard-value v)
   ;; TODO
   v)
-
-(define (stringify value)
-  (or
-   (and (string? value) value)
-   (format #f "~s" value)))
